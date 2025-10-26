350f14fc973f3fb7ce944bb6814545fae8e37459
nathyjoe001 <akpannath84@gmail.com>
2025-10-26 22:30:10 +0100

    update url

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -8,10 +8,10 @@ from .views import (
 urlpatterns = [
     # GET / POST /strings
     path("strings", StringListCreateView.as_view(), name="strings_list_create"),
-    
-    # GET / DELETE /strings/{string_value}
-    path("strings/<str:string_value>", StringDetailView.as_view(), name="strings_detail"),
-    
+
     # GET /strings/filter-by-natural-language
     path("strings/filter-by-natural-language", NaturalLanguageFilterView.as_view(), name="strings_filter_by_natural_language"),
+
+    # GET / DELETE /strings/{string_value}
+    path("strings/<str:string_value>", StringDetailView.as_view(), name="strings_detail"),
 ]
